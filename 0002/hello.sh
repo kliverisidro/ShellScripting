58df0a8cf3081020ceb7afb18bb5b6ba78f68090
Eyelson Isidro <eyelson.isidro@equifax.com>
2020-07-04 01:02:44 -0500

    Clase 0002 - Variables y comentarios

---
+++ 0002/hello.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# this is a comment
+echo "Hello World" #this is also a comment
+
+echo Our shell name is $BASH
+echo Our shell version name is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our current working directory is $PWD
+
+name=Kliver
+val=10
+echo The name is $name
+echo value $val
+#1val=11
+#10val=100
+VALUE=111
+#echo $1val
+#echo $10val
+echo $VALUE
