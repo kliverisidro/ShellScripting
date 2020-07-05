3badfe0c0b3346f1f1c50051ff02146b44611be2
Eyelson Isidro <eyelson.isidro@equifax.com>
2020-07-05 00:48:46 -0500

    Clase 0017 - Read a file content in Bash

---
+++ semana03/3/hello.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#while loops
+
+while read p
+do
+    echo $p
+done < hello.sh
+
+cat hello.sh | while read p
+do
+    echo $p
+done
+
+while IFS= read -r line
+do
+    echo $line
+done < /etc/host.conf
